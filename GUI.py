89570435e153be1f1079acaf483e03f160e6d6d1
triint <triintari@gmail.com>
2014-12-09 16:11:07 +0200

    esimese lehe ujundus ja pdf
    435

--- GUI.py
+++ GUI.py
@@ -42,53 +42,60 @@ kliendid = dr.outkliendid()
 tooted = dr.outtooted()
 
 #### kliendi valimine
+label1 = tk.Label(f1, text="Kliendi nimi:")
+label1.grid(column = 0, row = 4, padx=3, pady=5)
 
 klient = tk.StringVar(f1)
 klient.set(kliendid[0]) # default value
 
 w = tk.OptionMenu(f1, klient, *kliendid)
-w.pack(padx=3, pady=5)
+w.grid(column = 1, row = 4, padx=3, pady=5, )
 
 
 
 ###### toode 1
+label2 = tk.Label(f1, text="Toode")
+label2.grid(column = 1, row = 5, padx=10, pady=10)
 
 toode1 = tk.StringVar(f1)
 toode1.set(tooted[0]) # default value
 
 w2 = tk.OptionMenu(f1, toode1, *tooted)
-w2.pack(padx=3, pady=5)
+w2.grid(column =1, row = 6, padx=10, pady=10)
 
 
 ######### toode 1 kogus
 
 L1 = tk.Label(f1, text="Kogus")
-L1.pack()
+L1.grid(column =2, row = 5, padx=10, pady=10)
 E1 = tk.Entry(f1, bd =5)
-E1.pack()
+E1.grid(column =2, row = 6, padx=10, pady=10)
 
-######### toode 2
+########### toode 2
+
+label3 = tk.Label(f1, text="Toode")
+label3.grid(column = 1, row = 7, padx=10, pady=10)
 
 toode2 = tk.StringVar(f1)
 toode2.set(tooted[0]) # default value
 
 w3 = tk.OptionMenu(f1, toode2, *tooted)
-w3.pack(padx=3, pady=5)
+w3.grid(column =1, row = 8, padx=10, pady=10)
 
-######### toode 1 kogus
+
+######### toode 2 kogus
 
 L2 = tk.Label(f1, text="Kogus")
-L2.pack()
+L2.grid(column =2, row = 7, padx=10, pady=10)
 E2 = tk.Entry(f1, bd =5)
-E2.pack()
-
+E2.grid(column =2, row = 8, padx=10, pady=10)
 
 ##### loo arve nupp
 def ok():
     print("value is", klient.get())
 
 button = tk.Button(f1, text="Loo arve", command=lambda: loopdf("2", klient.get(), toode1.get(), toode2.get()))
-button.pack()
+button.grid(column =4, row = 10, padx=10, pady=10)
 
 
 ######################
--- pdfgen.py
+++ pdfgen.py
@@ -3,46 +3,49 @@ from reportlab.pdfgen import canvas
 
 def loopdf(arvenumber, kliendinimi, toode1, toode2):
     c = canvas.Canvas("arve" + arvenumber + ".pdf")
-    c.drawString(100,750,"FIRMANIMI")
-    c.drawString(100,675,'Arve nr:')
-    c.drawString(100,660,'Kuupäev:')
+    c.drawString(75,750,"FIRMANIMI")
+    c.drawString(75,675,'Arve nr:')
+    c.drawString(75,660,'Kuupäev:')
     
-    c.drawString(100,625,'Maksja:')
+    c.drawString(75,625,'Maksja:')
 
 
-    c.line(100,600,550,600)
-    c.drawString(100,575,'Toode')
-    c.drawString(275,575,'Kogus')
-    c.drawString(350,575,'Hind')
-    c.drawString(425,575,'Summa')
-    c.drawString(100,550,toode1)
-    c.drawString(275,550,'1 tk')
-    c.drawString(350,550,'4€/kg')
-    c.drawString(425,550,'4€')
-    c.drawString(100,530,toode2)
-    c.drawString(275,530,'1 tk')
-    c.drawString(350,530,'4€/kg')
-    c.drawString(425,530,'4€')
-
-    c.drawString(350,430,'KOKKU:')
-    c.drawString(425,430,'8€')
-
-    c.drawString(100,330,'Krista Teearu')
-    c.drawString(100,315,'Juhataja')
-
-    c.line(100,300,550,300)
-
-    c.drawString(100,275,'FIRMANIMI')
-    c.drawString(100,255,'Sipelga tn 8-61')
-    c.drawString(100,235,'Tallinn 34758')
-    c.drawString(100,215,'Harjumaa')
     
-    c.drawString(275,275,'Telefon: 538205')
-    c.drawString(275,255,'E-mail: jama@jama.ee')
-    c.drawString(275,235,'Reg nr: 73578358')
+    c.drawString(75,575,'Toode')
+    c.drawString(250,575,'Kogus')
+    c.drawString(325,575,'Hind')
+    c.drawString(400,575,'Summa')
     
-    c.drawString(450,275,'SEB')
-    c.drawString(450,255,'40385760774')
+    c.line(65,568,525,568)
+    
+    c.drawString(75,550,toode1)
+    c.drawString(250,550,'1 tk')
+    c.drawString(325,550,'4€/kg')
+    c.drawString(400,550,'4€')
+    c.drawString(75,530,toode2)
+    c.drawString(250,530,'1 tk')
+    c.drawString(325,530,'4€/kg')
+    c.drawString(400,530,'4€')
+
+    c.drawString(325,430,'KOKKU:')
+    c.drawString(400,430,'8€')
+
+    c.drawString(75,330,'Krista Teearu')
+    c.drawString(75,315,'Juhataja')
+
+    c.line(65,130,525,130)
+
+    c.drawString(75,105,'FIRMANIMI')
+    c.drawString(75,90,'Sipelga tn 8-61')
+    c.drawString(75,75,'Tallinn 34758')
+    c.drawString(75,60,'Harjumaa')
+    
+    c.drawString(240,105,'Telefon: 538205')
+    c.drawString(240,90,'E-mail: jama@jama.ee')
+    c.drawString(240,75,'Reg nr: 73578358')
+    
+    c.drawString(425,105,'SEB')
+    c.drawString(425,90,'40385760774')
     
 
     c.save()
